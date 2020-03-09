5259838837f37f0c4f31a30181f3329b2df1f00d
TripleThreads <programmers2017@outlook.com>
2020-03-09 11:34:25 +0300

    argon added

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <v-app>
-    <core-toolbar />
-
-    <core-drawer />
-
-    <core-view />
-
-    <core-footer />
-
-    <core-cta />
-  </v-app>
+    <div id="app">
+        <router-view name="header"></router-view>
+        <main>
+            <fade-transition origin="center" mode="out-in" :duration="250">
+                <router-view/>
+            </fade-transition>
+        </main>
+        <router-view name="footer"></router-view>
+    </div>
 </template>
-
 <script>
+import { FadeTransition } from "vue2-transitions";
+import VueCarousel from 'vue-carousel';
 
-  export default {
-    name: 'App',
-    components: {
-      CoreCta: () => import('~/components/core/Cta'),
-      CoreDrawer: () => import('~/components/core/Drawer'),
-      CoreFooter: () => import('~/components/core/Footer'),
-      CoreToolbar: () => import('~/components/core/Toolbar'),
-      CoreView: () => import('~/components/core/View')
-    },
-    data () {
-      return {
-      //
-      }
-    }
+
+export default {
+  components: {
+    FadeTransition
   }
+};
 </script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,9 @@ $config = [
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <title>{{ config('app.name') }}</title>
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/vuetify/2.1.1/vuetify.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
 </head>
 <body>
