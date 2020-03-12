b2fe922a82fa66cd7d48050e0f029d8dd36c7c3b
TripleThreads <programmers2017@outlook.com>
2020-03-12 12:19:26 +0300

    events

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import {setContent} from "./set-contents";
+
 const Home = () => import('~/views/Home.vue');
 const NewsEvents = () => import('~/components/news/Events');
 
@@ -9,7 +11,7 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {path: '/', name: 'home', component: Home},
-    {path: '/news/events', name: 'events', component: NewsEvents},
+    {path: '/', name: 'home', component: Home, beforeEnter: setContent},
+    {path: '/news/events', name: 'events', component: NewsEvents, beforeEnter: setContent},
   ]
 })
---
+++ resources/js/store/store.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+
+import events from "./event"
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      paths: ['user'],
+    })
+  ],
+
+  state: {
+    user: null
+  },
+  getters: {
+    getApiToken: state => {
+      if (state.user === null)
+        return null;
+      return state.user.token;
+    },
+  },
+  mutations: {
+    resetUser: (state) => {
+      state.user = null;
+    },
+    setUser: (state, payload) => {
+      state.user = payload;
+    }
+  },
+
+  actions: {
+    setUser: ({commit}, payload) => {
+      commit('setUser', payload);
+    },
+    resetUser: ({commit}) => {
+      commit("resetUser");
+    }
+  },
+  modules: {
+    events
+  }
+});
