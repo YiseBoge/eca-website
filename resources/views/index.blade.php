e621f8cdcc7bbd5acd312ad8e9f4eb087e950819
yiseboge <Ethiopia1!>
2020-04-13 22:49:11 +0300

    remove bootstrap js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,9 +30,6 @@ $config = [
 </script>
 
 {{-- Load the application scripts --}}
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
 <script src="{{ asset('dist/js/app.js') }}"></script>
 </body>
 </html>
