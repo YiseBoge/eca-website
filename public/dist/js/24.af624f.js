da0426bb1477a234dd2e18501a5c29475067c50d
yiseboge <Ethiopia1!>
2020-04-15 23:16:27 +0300

    git not ignore dist

---
+++ public/dist/js/24.af624f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{277:function(t,e,n){"use strict";n.r(e);var r=n(2),o=Object(r.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("v-content",{attrs:{id:"home"}},[e("router-view")],1)}),[],!1,null,null,null);e.default=o.exports}}]);
