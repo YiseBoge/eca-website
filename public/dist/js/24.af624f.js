fa97b87ad572e954d8f39a33203d22a7277f623e
yiseboge <Ethiopia1!>
2020-04-16 00:08:53 +0300

    enable nprogress

--- public/dist/js/24.af624f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{277:function(t,e,n){"use strict";n.r(e);var r=n(2),o=Object(r.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("v-content",{attrs:{id:"home"}},[e("router-view")],1)}),[],!1,null,null,null);e.default=o.exports}}]);
