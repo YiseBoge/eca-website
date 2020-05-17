c51de204e8abef38121debb717cbea24bb26c05f
Lydia <lydiagashawtena@gmail.com>
2020-05-17 21:16:02 +0300

    Implement search on backend

---
+++ app/Http/Resources/SearchItemResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SearchItemResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'type' => $this->type,
+            'item_id' => $this->item_id, 
+            'title' => $this->title,                       
+            'description' => $this->description,
+            'category' => $this->category,
+            'date_created' => $this->date
+        ];
+    }
+}
